1659d337e4a2586a47d3d9f823270ed1b385c3dc
renanz <renan_zelaya@unitec.edu>
2018-05-17 13:00:19 -0600

    Corrected logo and sizes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,16 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file .env file.
+
+Area.create!([{
+    area_id_number: "1-01",
+    name: "Departamentos Administrativos",
+  },
+  {
+    area_id_number: "1-10",
+    name: "Proceso de Aceite Crudo",
+  },
+  {
+    area_id_number: "1-30",
+    name: "Costos indirectos (extractora)",
+  }])
