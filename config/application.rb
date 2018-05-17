d7ed62b6c7611d3f555e3e6f8b4a188fb3b0155d
renanz <renan_zelaya@unitec.edu>
2018-05-17 13:38:11 -0600

    cambios alejandro

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,6 @@ module AdminlteRailsTemplate
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :es
   end
 end
