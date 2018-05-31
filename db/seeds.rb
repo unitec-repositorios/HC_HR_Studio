5dad3a7efb6250e379855534e0e3a3c3b718b529
renanz <renan_zelaya@unitec.edu>
2018-05-30 18:13:44 -0600

    changes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,72 +2,44 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 user = CreateAdminService.new.call
-#puts 'CREATED ADMIN USER: ' << user.email
+puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file .env file.
 
 Area.create!([{
-  area_id_number: "1-01",
-  name: "Departamentos Administrativos",
+  area_id_number: "1",
+  name: "Area1",
 },
 {
-  area_id_number: "1-10",
-  name: "Proceso de Aceite Crudo",
-},
-{
-  area_id_number: "1-30",
-  name: "Costos indirectos (extractora)",
+  area_id_number: "2",
+  name: "Area2",
 }])
 
 Education.create!([{
-  major_description: "Ingeniero Industrial o Carrera a Fin"
+  major_description: "Educacion 1"
 },
 {
-  major_description: "Licenciado en Administración de Empresas o Ingeniero Industrial."
-},
-{
-  major_description: "Secundaria Completa ( Perito Mercantil preferentemente)"
-},
-{
-  major_description: "Secundaria Completa"
-},
-{
-  major_description: "Ingeniero En El Área Industria, Ambiental O Químico; Preferiblemente Orientado En El Sector De Palma Africana Y En Temas Ambientales.  "
-},
-{
-  major_description: "Primaria Completa"
-},
-{
-  major_description: "Pasante de carrera universitaria (administración, producción, agrícola, etc.)"
-},
-{
-  major_description: "Que tenga licencia para conducir carro liviano"
-},
-{
-  major_description: "Técnico en mecánica, soldadura Industrial, graduado o con experiencia comprobada"
-},
-{
-  major_description: "Técnico en Mecánica Industrial, Técnico Electricista o carrera afín."
+  major_description: "Educacion 2"
 }])
 
 School.create!([{
-  name: "UNAH-VS",
+  name: "Institucion 1",
   cellphone: 0,
-  address: "Col. Universidad"
+  address: ""
 }])
 
 Ability.create!([{
-  ability_name: "Relaciones interpersonales"
+  ability_name: "Habilidad 1"
 }])
 
 Department.create!([{
-  area_id: "1-01",
-  name: "Asamblea general",
-  department_id_number: "01-01"
+  area_id: "1",
+  name: "Departamento 1",
+  department_id_number: "1"
 },
 {
-  area_id: "1-30",
-  name: "Gerencia ambiental",
-  department_id_number: "30-13"
+  area_id: "2",
+  name: "Departamento 2",
+  department_id_number: "2"
 }])
 
 
